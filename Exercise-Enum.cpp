d447b178bc8763287781c298e7b142d2ad28e1c6
maizuddin35 <maizuddin35@gmail.com>
2018-01-30 09:54:08 +0800

    Added exercise stringtype and enum

---
+++ Exercise-Enum.cpp
@@ -0,0 +1,11 @@
+
+
+//if the day is sunday, what day is it?
+//write your code using enum and Sunday is your default 'day'
+
+minggu hariIni;
+
+cout<<"Hari ke-"hariIni+1;
+return 0;
+
+
---
+++ Exercise-StringType.cpp
@@ -0,0 +1,34 @@
+//compare two strings
+//complete the program below. 
+
+#include <iostream>
+
+int main()
+{
+
+	string myName="Mike";//write your name there.
+	//make a while loop until user input 'quit' to exit/end the program
+
+	(true)
+	{
+		
+		string userName;
+		//get the user input for string.
+		if(userName=="Abu")
+	       	{
+			cout<<"Hey Abu!"<<endl;
+		}
+		else if(userName=="quit")
+		{
+			cout<<endl;
+			break;
+		}
+		else
+		{
+			cout<<"Aha! its "<<myName<<endl;
+		}
+	}
+	return 0;
+}
+
+
