4a82268e78d5ab8bf651eb53844c52858a7f7f51
umisuhanarahman <33883955+umisuhanarahman@users.noreply.github.com>
2018-02-05 10:03:11 +0800

    task completed

--- Exercise-StringType.cpp
+++ Exercise-StringType.cpp
@@ -1,34 +1,42 @@
+//umi suhana bt abdul rahman
+//a17dw2041
+
 //compare two strings
 //complete the program below. 
 
 #include <iostream>
+using namespace std;
 
 int main()
 {
 
-	string myName="Mike";//write your name there.
+	string myName="Hana";//write your name there.
 	//make a while loop until user input 'quit' to exit/end the program
 
-	(true)
+	while(true)
 	{
 		
 		string userName;
-		//get the user input for string.
+		
+		cout<<"Enter your username: ";
+		cin>>userName;   //get the user input for string.
 		if(userName=="Abu")
-	       	{
-			cout<<"Hey Abu!"<<endl;
+	    {
+			cout<<"Hey Abu, welcome back!"<<endl;
 		}
 		else if(userName=="quit")
 		{
 			cout<<endl;
 			break;
 		}
+		else if(userName!=myName)
+		{
+			cout<<"Hello "<<userName<<endl;
+		}
 		else
 		{
-			cout<<"Aha! its "<<myName<<endl;
+			cout<<"Aha! its you "<<myName<<endl;
 		}
 	}
 	return 0;
 }
-
-
